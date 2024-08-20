e076605172c8a85b22c0789043e82e08d1fb8adc
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-08-20 22:30:13 +0700

    update repo

--- payment-service/src/main/java/com/main/paymentservice/repository/TransactionRepository.java
+++ payment-service/src/main/java/com/main/paymentservice/repository/TransactionRepository.java
@@ -1,4 +1,12 @@
 package com.main.paymentservice.repository;
 
-public interface TransactionRepository {
+import com.main.paymentservice.models.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+
+    List<Transaction> findAllByTransactionStatus(String status);
+
 }
--- payment-service/src/main/java/com/main/paymentservice/repository/WalletRepository.java
+++ payment-service/src/main/java/com/main/paymentservice/repository/WalletRepository.java
@@ -1,4 +1,12 @@
 package com.main.paymentservice.repository;
 
-public interface WalletRepository {
+import com.main.paymentservice.models.Wallet;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface WalletRepository extends MongoRepository<Wallet, String> {
+
+    Optional<Wallet> findByAccountID(long accountId);
+
 }
