461c73e574406d629186b8b3e7052f413bfbebc1
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-09-16 22:48:07 +0700

    reupdate feign aspect

--- api-gateway/src/main/java/com/main/apigateway/aspects/FeignAspects.java
+++ api-gateway/src/main/java/com/main/apigateway/aspects/FeignAspects.java
@@ -1,4 +1,26 @@
 package com.main.apigateway.aspects;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
 public class FeignAspects {
+
+    @Pointcut("execution(* com.main.apigateway.client.PaymentClient.handlePaymentRequest(..))")
+    public void modifyPaymentRequest() {
+    }
+
+    @Around("modifyPaymentRequest()")
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        final var args = joinPoint.getArgs();
+        final var paymentRequest = (com.main.apigateway.models.dto.request.PaymentRequest) args[0];
+//        UserDet
+
+    }
 }
--- api-gateway/src/main/java/com/main/apigateway/client/PaymentClient.java
+++ api-gateway/src/main/java/com/main/apigateway/client/PaymentClient.java
@@ -1,4 +1,29 @@
 package com.main.apigateway.client;
 
+import com.main.apigateway.client.PaymentClient.FallbackForPaymentService;
+
+import com.main.apigateway.models.dto.request.PaymentRequest;
+import com.main.apigateway.models.dto.response.PaymentResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(value = "payment-service", fallback = FallbackForPaymentService.class)
 public interface PaymentClient {
+
+    @PostMapping("/payment")
+    ResponseEntity<PaymentResponse> handlePaymentRequest(@RequestBody PaymentRequest paymentRequest);
+
+    @Component
+    class FallbackForPaymentService implements PaymentClient {
+        @Override
+        public ResponseEntity<PaymentResponse> handlePaymentRequest(PaymentRequest paymentRequest) {
+            return ResponseEntity.ok(
+                    new PaymentResponse("Our payment service is currently unavailable. Please try again later.")
+            );
+        }
+    }
+
 }
