241f1b145975a6cb02d78547d104205613772d81
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-09-21 16:27:50 +0700

    Config security (Done)

--- api-gateway/src/main/java/com/main/apigateway/security/services/UserDetailsServiceImpl.java
+++ api-gateway/src/main/java/com/main/apigateway/security/services/UserDetailsServiceImpl.java
@@ -1,13 +1,27 @@
 package com.main.apigateway.security.services;
 
+import com.main.apigateway.models.Account;
+import com.main.apigateway.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-//    private final AccountRe
-    
+    private final AccountRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
 }
