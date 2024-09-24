ec5b0919661922d15ea14f3160ec41b6141d0056
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-09-24 21:31:42 +0700

    finish cmd main

---
+++ query-service/cmd/handlers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"query-service/internal/app/service"
+)
+
+func GetLeadFromRedis() gin.H {
+	leaderboard := service.GetLeaderboard(0, 10)
+
+	return gin.H{
+		"leaderboard": leaderboard,
+	}
+}
