e7b73a4b2a4b8fe2135427dd4d7653bac83a23b8
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-10-13 09:09:40 +0700

    Add Models, Repositories to Main Class

--- api-gateway/src/main/java/com/main/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/main/apigateway/ApiGatewayApplication.java
@@ -6,12 +6,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableJpaRepositories(basePackages = "com.main.apigateway.repository")
+@EntityScan(basePackages = "com.main.apigateway.models")
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
